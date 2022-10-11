9ac85b0d9889908ae45d706d4183e832df21ce4b
Joel Lundin <joel.lundin@gmail.com>
2022-10-11 16:13:22 +0200

    Add influxdb integration

--- app.py
+++ app.py
@@ -1,50 +1,61 @@
+from http import client
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
-from influxdb import InfluxDBClient
 import time
+import datetime
+import os
+
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-url = "https://elen.nu/timpriser-pa-el-for-elomrade-se3-stockholm/"
-req = requests.get(url)
-soup = BeautifulSoup(req.text, "html.parser")
+token = os.getenv("TOKEN")
+org = os.getenv("ORG")
+bucket = os.getenv("BUCKET")
 
-client = InfluxDBClient(host='localhost', port=8086)
 
-tables = soup.find_all('table')
+def connect_to_influxdb():
+    client = InfluxDBClient(url=os.getenv("URL"), token=token)
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+    return client, write_api
 
-table = soup.find('table', class_="w-full px-4")
+def get_data(client, write_api):
+    url = "https://elen.nu/timpriser-pa-el-for-elomrade-se3-stockholm/"
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, "html.parser")
 
-data = {
-    'Date': [],
-    'Time': [],
-    'Price/SEK': []}
+    tables = soup.find_all('table')
+    table = soup.find('table', class_="w-full px-4")
 
-for row in table.tbody.find_all('tr'):
+    format = "%Y-%m-%d %H:%M"
+
+    for row in table.tbody.find_all('tr'):
     #print(row)
 
-    columns = row.find_all('td')
-    #print(columns)
+        columns = row.find_all('td')
+        #print(columns)
 
-    if(columns != []):
-        datetime = columns[0].text.strip()
-        date = datetime.split()[0]
-        time = datetime.split()[1]
-        rawprice = columns[1].text.strip()
-        price = float(rawprice.split()[0].replace(',', '.'))
+        if(columns != []):
+            dt_string = columns[0].text.strip()
+            rawprice = columns[1].text.strip()
+            price = float(rawprice.split()[0].replace(',', '.'))
 
-        data['Date'].append(date)
-        data['Time'].append(time)
-        data['Price/SEK'].append(price)
+            dt_object = datetime.datetime.strptime(dt_string, format)
 
-df = pd.DataFrame.from_dict(data)
+            point = Point("SpotPrice")\
+                .tag("Region", "s3")\
+                .field("price", price)\
+                .time(dt_object, WritePrecision.S)
 
-df = df.set_index('Date')
+            write_api.write(bucket, org, point)
 
-df.plot(x='Time', y='Price/SEK')
 
-plt.plot(df['Time'], df['Price/SEK'])
+if __name__=="__main__":
+    client, write_api = connect_to_influxdb()
+    get_data(client, write_api)
+    print("Done!")
